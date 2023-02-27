ebf613774ee1633c8319c106a7099d7c55fb7532
m-njora <mmuthekijora@gmail.com>
2023-02-28 00:13:53 +0300

    aggregate methods

--- app/product.rb
+++ app/product.rb
@@ -1,5 +1,17 @@
 class Product < ActiveRecord::Base
     has_many :reviews
     has_many :users, through: :reviews
+    def leave_review(user,star_rating,comment)
+        self.reviews.create(user,star_rating,comment)
+    end
 
+    def print_all reviews
+        self.reviews.all.map do |review|
+            "Review for #{self.title} by #{t.user.name}: #{t.star_rating}.#{t.comment}"
+    end
+end
+def average_rating
+    self.reviews.average(:star_rating).to_f
+
+end
 end
--- db/migrate/20230227202238_create_products.rb
+++ db/migrate/20230227202238_create_products.rb
@@ -1,4 +1,4 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
-  def change
+  def change :products do |t|
   end
 end
