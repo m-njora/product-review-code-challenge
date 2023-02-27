ffe443fc7cdfc1d5dee39fbb77449a80567c6738
m-njora <mmuthekijora@gmail.com>
2023-02-28 00:32:44 +0300

    done

--- app/user.rb
+++ app/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
     has_many :reviews
     has_many :products,through: :reviews
+
+    def favourite_product
+       self.reviews.maximum(:star_rating)
+    end
+
+    def remove_reviews(product)
+        self.reviews.destroy_by(product: product)
+    end
 end
